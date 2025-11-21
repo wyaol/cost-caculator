1754437de11be975da74bba5481fc216522e0d85
yao_wang <289672494@qq.com>
2025-11-21 16:10:47 +0800

    yao[feat]: rename NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -9,5 +9,5 @@ export const supabasePublic = createClient(
 
 export const supabaseServer = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.SUPABASE_SERVICE_ROLE_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY!
 );
